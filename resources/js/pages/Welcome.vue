acd967c625fc01a45366426c6ecf7bd51baf7a98
vaclova2020 <vaclovas.lapinskis@gmail.com>
2025-07-25 20:26:30 +0300

    update welcome

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -4,10 +4,7 @@ import { Head, Link } from '@inertiajs/vue3';
 
 <template>
 
-    <Head title="Welcome">
-        <link rel="preconnect" href="https://rsms.me/" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Head>
+    <Head title="ICE Poker for Small IT Teams" />
     <div
         class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
         <header class="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
@@ -91,7 +88,7 @@ import { Head, Link } from '@inertiajs/vue3';
                         <li>
                             <a :href="route('register')"
                                 class="inline-block rounded-sm border border-black bg-[#1b1b18] px-5 py-1.5 text-sm leading-normal text-white hover:border-black hover:bg-black dark:border-[#eeeeec] dark:bg-[#eeeeec] dark:text-[#1C1C1A] dark:hover:border-white dark:hover:bg-white">
-                                Get satarted
+                                Get started
                             </a>
                         </li>
                     </ul>
