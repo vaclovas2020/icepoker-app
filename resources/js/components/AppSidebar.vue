b090dbefcc1d7bf7f7337c0e33026de9fdd113d3
vaclova2020 <vaclovas.lapinskis@gmail.com>
2025-07-25 19:40:28 +0300

    add my github repo to sidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -19,14 +19,9 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/vaclovas2020/icepoker-app',
         icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
+    }
 ];
 </script>
 
